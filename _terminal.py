e26a3e7abd08213d2e0a8325716250cffea9e434
Cédric Chépied <cedric.chepied@gmail.com>
2016-09-16 10:49:07 +0200

    (dé)montage usb terminal

--- _terminal.py
+++ _terminal.py
@@ -7,8 +7,6 @@ grammar = Grammar(u'Grammaire terminal')
 
 class ChepTerminal(MappingRule):
 	mapping = {
-		u'liste fichiers': Text(u'ls') + Key(u'enter'),
-		u'liste complète': Text(u'ls -lh') + Key(u'enter'),
 		u'terminal suivant': Key(u's-right'),
 		u'terminal précédent': Key(u's-left'),
 		u'nouveau terminal': Key(u'cs-t'),
@@ -21,8 +19,31 @@ class ChepTerminal(MappingRule):
 		'n': 1,
 	}
 
+class ChepCommandes(MappingRule):
+	mapping = {
+		u'liste fichiers': Text(u'ls') + Key(u'enter'),
+		u'liste complète': Text(u'ls -lh') + Key(u'enter'),
+		u'monte usb <lettre> [<n>]': Text(u'mount /dev/sd'
+		                                  + u'%(lettre)s'.lower()
+		                                  + u'%(n)d') + Key(u'enter'),
+		u'démonte usb': Text(u'umount /media/usb') + Key(u'enter'),
+	}
+	extras = [
+		Choice(u'lettre', { u'a': u'a',
+		                    u'b': u'b',
+		                    u'c': u'c',
+		                    u'd': u'd',
+		                  }
+		),
+		Integer('n', 0, 10000000),
+	]
+	defaults = {
+		'n': 1,
+	}
+
 
 grammar.add_rule(ChepTerminal())
+grammar.add_rule(ChepCommandes())
 
 grammar.load()
 
