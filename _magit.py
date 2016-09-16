acc7c002e907129d0c17b0ae72d19d7a30f9d266
Cédric Chépied <cedric.chepied@gmail.com>
2016-09-16 10:56:59 +0200

    magit plus facile à comprendre

--- _magit.py
+++ _magit.py
@@ -7,7 +7,7 @@ grammar = Grammar('Grammaire magit')
 
 class ChepMagit(MappingRule):
 	mapping = {
-		u'maguit': Key(u'c-c,m,s'),
+		u'git status': Key(u'c-c,m,s'),
 		u'commit': Key(u'c-c,c-c'),
 		u'commit svn': Key(u'a-x') + Text(u'magit-svn-dcommit') + Key('enter'),
 	}
